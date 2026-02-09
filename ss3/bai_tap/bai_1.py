a2195210115cd509e851843cf1aefaeb13039700
Nttt1904 <132934830+Nttt1904@users.noreply.github.com>
2026-02-09 21:23:45 +0700

    Bài tập 1

---
+++ ss3/bai_tap/bai_1.py
@@ -0,0 +1,10 @@
+name = "TriemNHN"
+age = "27"
+hobby = "play game"
+favorite_subject = "English"
+
+print ("Tên: " + name)
+print ("Tuổi: " + age)
+print ("Sở thích: "+ hobby)
+print ("Môn học ưu thích: "+ favorite_subject)
+
