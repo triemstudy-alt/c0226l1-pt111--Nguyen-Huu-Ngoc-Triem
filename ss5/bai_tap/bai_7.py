43dfa025718ad4e68507afefb6e7423412ba20bb
Nttt1904 <132934830+Nttt1904@users.noreply.github.com>
2026-02-13 20:36:44 +0700

    Update bài 7 buổi 5

--- ss5/bai_tap/bai_7.py
+++ ss5/bai_tap/bai_7.py
@@ -1,10 +1,6 @@
 year = int(input("Nhập vào năm: "))
 
-if year%4 == 0 and year%100 != 0 :
-    print("Năm nhuận")
-elif year%100 == 0 and year%400 != 0:
-    print("Không phải năm nhuận")
-elif year%100 == 0 and year%400 == 0:
+if (year%4 == 0 and year%100 != 0) or (year%400 == 0):
     print("Năm nhuận")
 else :
     print("Không phải năm nhuận")
