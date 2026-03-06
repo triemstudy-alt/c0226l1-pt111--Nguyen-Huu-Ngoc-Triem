e4de66527a57734a9673a4a59a5ef5f96c89b994
TriemNHN <triemstudy@gmail.com>
2026-03-06 21:27:13 +0700

    Bài 13 -6

--- ss13/bai_tap/bai6.py
+++ ss13/bai_tap/bai6.py
@@ -1,3 +1,4 @@
+isDup = False
 number1 = []
 for i in range(10):
     number1.append(int(input("Nhập vào data trong list: ")))
@@ -5,3 +6,7 @@ for i in range(10):
 number2 = []
 for i in range(10):
     number1.append(int(input("Nhập vào data trong list: ")))
+
+for i in range(len(number1)):
+    if number1(i) == number2[i] and len(number1) == len(number2):
+        isDup == True
